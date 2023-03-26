199e67691b5c2aa034a0c190763e6c9f54d95b5f
Justin Chen <yeutyngchen@gmail.com>
2023-03-26 11:06:35 -0400

    Added arduino and C# source code

---
+++ Program.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using OpenHardwareMonitor.Hardware;
+using System.IO.Ports;
+using System.Threading;
+using Sharer;
+using System.Collections.Generic;
+
+
+namespace PPSU_hwmonitor_c
+{
+    class Program
+    {
+        /**
+         *  Define vars to hold stats
+         **/
+
+        // CPU Temp
+        static float cpuTemp;
+        // CPU Usage
+        static float cpuUsage;
+        // CPU Power Draw (Package)
+        static float cpuPowerDrawPackage;
+        // CPU Frequency
+        static float cpuFrequency;
+        // GPU Temperature
+        static float gpuTemp;
+        // GPU Usage
+        static float gpuUsage;
+        // GPU Core Frequency
+        static float gpuCoreFrequency;
+        // GPU Memory Frequency
+        static float gpuMemoryFrequency;
+
+        // create a new SerialPort object
+        //SerialPort serialPort = new SerialPort("COM5", 9600);
+
+
+
+        /**
+         * Init OpenHardwareMonitor.dll Computer Object
+         **/
+
+        static Computer c = new Computer()
+        {
+            GPUEnabled = true,
+            CPUEnabled = true,
+            //RAMEnabled = true, // uncomment for RAM reports
+            //MainboardEnabled = true, // uncomment for Motherboard reports
+            //FanControllerEnabled = true, // uncomment for FAN Reports
+            //HDDEnabled = true, // uncomment for HDD Report
+        };
+
+        /**
+         * Pulls data from OHM
+         **/
+
+        static String ReportSystemInfo()
+        {   //set which values to send to serial monitor
+            //Must set less than or equal to 4 values as true
+            bool GPUTemp = false; //**testing computer does not have independent GPU
+            bool GPUUsage = false;
+            bool GPUFrequency = false;
+            bool GPUMemory = false;
+
+            bool CPUTemp = true;
+            bool CPUUsage = true;
+            bool CPUPackage = true;
+            bool CPUFrequency = true;
+
+            int setvals = GPUTemp ? 1 : 0;
+            setvals += GPUUsage ? 1 : 0;
+            setvals += GPUFrequency ? 1 : 0;
+            setvals += GPUMemory ? 1 : 0;
+            setvals += CPUTemp ? 1 : 0;
+            setvals += CPUUsage ? 1 : 0;
+            setvals += CPUPackage ? 1 : 0;
+            setvals += CPUFrequency ? 1 : 0;
+            System.Diagnostics.Debug.WriteLine("setvals: " + setvals);
+            if(setvals > 4)
+            {
+                return "#More than 4 values were set to be viewed. Please keep them below or equal to 4$#$#$#$";
+            }
+
+            String data = ""; //will hold serial data
+            foreach (var hardware in c.Hardware)
+            {
+
+                if (hardware.HardwareType == HardwareType.CPU)
+                {
+                    // only fire the update when found
+                    hardware.Update();
+
+                    // loop through the data
+                    foreach (var sensor in hardware.Sensors)
+                        if (sensor.SensorType == SensorType.Temperature && sensor.Name.Contains("CPU Package") && CPUTemp)
+                        {
+                            // store
+                            cpuTemp = sensor.Value.GetValueOrDefault();
+                            // print to console and append to data
+                            float val = sensor.Value.GetValueOrDefault();
+                            System.Diagnostics.Debug.WriteLine("cpuTemp: " + val);
+                            data += " $" + int.Parse(val.ToString().Split('.')[0]);
+                        }
+                        else if (sensor.SensorType == SensorType.Load && sensor.Name.Contains("CPU Total") && CPUUsage)
+                        {
+                            // store
+                            cpuUsage = sensor.Value.GetValueOrDefault();
+                            // print to console and append to data
+                            float val = sensor.Value.GetValueOrDefault();
+                            System.Diagnostics.Debug.WriteLine("cpuUsage: " + val);
+                            data += " $" + int.Parse(val.ToString().Split('.')[0]);
+                        }
+                        else if (sensor.SensorType == SensorType.Power && sensor.Name.Contains("CPU Package") && CPUPackage)
+                        {
+                            // store
+                            cpuPowerDrawPackage = sensor.Value.GetValueOrDefault();
+                            // print to console and append to data
+                            float val = sensor.Value.GetValueOrDefault();
+                            System.Diagnostics.Debug.WriteLine("CPU Power Draw - Package: " + val);
+                            data += " $" + int.Parse(val.ToString().Split('.')[0]);
+
+                        }
+                        else if (sensor.SensorType == SensorType.Clock && sensor.Name.Contains("CPU Core #1") && CPUFrequency)
+                        {
+                            // store
+                            cpuFrequency = sensor.Value.GetValueOrDefault();
+                            // print to console and append to data
+                            float val = sensor.Value.GetValueOrDefault();
+                            System.Diagnostics.Debug.WriteLine("cpuFrequency: " + val);
+                            data += " $" + int.Parse(val.ToString().Split('.')[0]);
+                        }
+                }
+
+
+                // Targets AMD & Nvidia GPUS
+                if (hardware.HardwareType == HardwareType.GpuAti || hardware.HardwareType == HardwareType.GpuNvidia)
+                {
+                    // only fire the update when found
+                    hardware.Update();
+
+                    // loop through the data
+                    foreach (var sensor in hardware.Sensors)
+                        if (sensor.SensorType == SensorType.Temperature && sensor.Name.Contains("GPU Core") && GPUTemp)
+                        {
+                            // store
+                            gpuTemp = sensor.Value.GetValueOrDefault();
+                            // print to console and append to data
+                            float val = sensor.Value.GetValueOrDefault();
+                            System.Diagnostics.Debug.WriteLine("gpuTemp: " + val);
+                            data += "#GPUTemp: $" + int.Parse(val.ToString().Split('.')[0]);
+                        }
+                        else if (sensor.SensorType == SensorType.Load && sensor.Name.Contains("GPU Core")&&GPUUsage)
+                        {
+                            // store
+                            gpuUsage = sensor.Value.GetValueOrDefault();
+                            // print to console and append to data
+                            float val = sensor.Value.GetValueOrDefault();
+                            System.Diagnostics.Debug.WriteLine("gpuUsage: " + val);
+                            data += "#GPUUsage: $" + int.Parse(val.ToString().Split('.')[0]);
+                        }
+                        else if (sensor.SensorType == SensorType.Clock && sensor.Name.Contains("GPU Core")&&GPUFrequency)
+                        {
+                            // store
+                            gpuCoreFrequency = sensor.Value.GetValueOrDefault();
+                            // print to console and append to data
+                            float val = sensor.Value.GetValueOrDefault();
+                            System.Diagnostics.Debug.WriteLine("gpuCoreFrequency: " + val);
+                            data += "#GPUCoreFreq: $" + int.Parse(val.ToString().Split('.')[0]);
+                        }
+                        else if (sensor.SensorType == SensorType.Clock && sensor.Name.Contains("GPU Memory") && GPUMemory)
+                        {
+                            // store
+                            gpuMemoryFrequency = sensor.Value.GetValueOrDefault();
+                            // print to console and append to data
+                            float val = sensor.Value.GetValueOrDefault();
+                            System.Diagnostics.Debug.WriteLine("gpuMemoryFrequency: " + val);
+                            data += "#GPUMemFreq: $" + int.Parse(val.ToString().Split('.')[0]);
+                        }
+
+                }
+
+                // Can also gather fan data, motherboard, ram, and HDD data
+
+            }
+            return data;
+        }
+
+        static void Main(string[] args)
+        {
+            //Mutex mutex = new Mutex(false, "MySerialPortLock");
+            var connection = new SharerConnection("COM5", 9600);
+            connection.Connect();
+            
+
+            // loop
+            while (true)
+            {
+
+                
+                c.Open();
+                // send a string to the serial port
+                string message = ReportSystemInfo();
+                //SerialPort serialPort = new SerialPort("COM5", 9600);
+                //serialPort.DtrEnable = true;
+                //serialPort.RtsEnable = true;
+                //System.Diagnostics.Debug.WriteLine(message);
+
+                // open the serial port
+                //serialPort.Open();
+
+                //serialPort.Write('1');
+                connection.RefreshFunctions();
+                connection.WriteVariable("MyVar", message); //send data containing parseable CPU/GPU stats
+                // close the serial port
+                //serialPort.Close();
+                //return;
+                //Thread.Sleep(2000);
+                //mutex.ReleaseMutex();
+            }
+        }
+    }
+}
---
+++ hwdisplay.ino
@@ -0,0 +1,122 @@
+#include <LiquidCrystal.h>// include the library code
+#include <Wire.h>
+#include <Sharer.h> //library for serial connections
+
+
+//
+char array1[16];  //the string to print on the LCD
+char array2[16];  //the string to print on the LCD
+int tim = 190;  //the value of delay time
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(4, 6, 10, 11, 12, 13);
+//
+void setup()
+{
+  
+  lcd.begin(16, 2);  // set up the LCD's number of columns and rows: 
+  Serial.begin(9600); // initialize serial communication at 9600 bits per second
+  Sharer.init(9600);
+}
+//
+int values[4]; // an array to store the 4 stat
+
+
+void loop() 
+{
+   Serial.println(values[3]);
+   String inputString = " $10 $60 $10 $10";//the imported 4 values with symbol in between in between
+   Sharer.run();
+  Sharer.read();
+  String str = Sharer.readString();
+  Sharer.flush();
+  Serial.print(str);
+  inputString = str;
+  //char array3[15];
+    //sprintf(array3, "%s", str);
+   //lcd.print(array3);
+  
+  //while (Serial.available() > 0) { // check if there's data available on the serial port
+    
+    //inputString = Serial.readString(); // read the incoming data and store it in a variable
+    //inputString = " $20 $60 $20 $20";
+    //Serial.print("Received: "); // print a message to the serial monitor
+    //Serial.println(inputString); // print the incoming data to the serial monitor
+  //}
+  
+  // Parse and split the input string into 4 substrings using the space character as the delimiter
+  String substrings[4];
+  int startIndex = inputString.indexOf('$', 0);
+  for (int i = 0; i < 4; i++) {
+    int spaceIndex = inputString.indexOf('$', startIndex+1);
+      substrings[i] = inputString.substring(startIndex+1, spaceIndex);
+      startIndex = spaceIndex;
+
+  }
+
+  // Convert each substring to an integer using parseInt() and store the results in the values array
+  for (int i = 0; i < 4; i++) {
+    values[i] = substrings[i].toInt();
+  }
+
+  // Use sprintf() to format the first two integers with a space character in between and concatenate them into the first char array
+  sprintf(array1, "%s %d, %s %d", "CPU", values[0], "Temp", values[1]);
+
+  // Use sprintf() to format the last two integers with a space character in between and concatenate them into the second char array
+  sprintf(array2, "%s %d, %s %d", "GPU", values[2], "Temp", values[3]);
+
+  int num1 = values[0];
+  int num2 = values[1];
+  int num3 = values[2];
+  int num4 = values[3]; 
+
+  // Set cursor to blink if num > 90
+  while (num1 > 90 || num2 > 50 || num3 > 90 || num4 > 50) {
+    lcd.blink(); // turn on cursor blinking
+    delay(500); // wait for 2000ms
+    lcd.noBlink(); // turn off cursor blinking
+    lcd.setCursor(0,0);  // set the cursor to column 0, line 0
+    lcd.print(array1);  // Print a message to the LCD.
+   lcd.setCursor(0,1);  // set the cursor to column 0, line 1
+    lcd.print(array2);
+    delay(2000); // wait for 2000ms
+    lcd.setCursor(0,0);  // set the cursor to column 0, line 0
+    lcd.clear();
+  String str = Sharer.readString();
+  // Parse and split the input string into 4 substrings using the space character as the delimiter
+  String substrings[4];
+  int startIndex = inputString.indexOf('$', 0);
+  for (int i = 0; i < 4; i++) {
+    int spaceIndex = inputString.indexOf('$', startIndex+1);
+      substrings[i] = inputString.substring(startIndex+1, spaceIndex);
+      startIndex = spaceIndex;
+
+  }
+
+  // Convert each substring to an integer using parseInt() and store the results in the values array
+  for (int i = 0; i < 4; i++) {
+    values[i] = substrings[i].toInt();
+  }
+    num1 = values[0];
+    num2 = values[1];
+    num3 = values[2];
+    num4 = values[3];
+      // Use sprintf() to format the first two integers with a space character in between and concatenate them into the first char array
+  sprintf(array1, "%s %d, %s %d", "CPU", values[0], "Temp", values[1]);
+
+  // Use sprintf() to format the last two integers with a space character in between and concatenate them into the second char array
+  sprintf(array2, "%s %d, %s %d", "GPU", values[2], "Temp", values[3]);
+  }
+
+    lcd.print(array1);  // Print a message to the LCD.
+   lcd.setCursor(0,1);  // set the cursor to column 0, line 0
+    lcd.print(array2);
+    delay(2000); // wait for 2000ms
+    lcd.setCursor(0,0);  // set the cursor to column 0, line 0
+
+    num1 = values[0];
+    num2 = values[1];
+    num3 = values[2];
+    num4 = values[3];
+
+}
+//
